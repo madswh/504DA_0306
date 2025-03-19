3cf25f64e2c1f20282229429e0a91ff0d39353eb
madswh <whitmi000@gmail.com>
2025-03-19 10:20:14 -0700

    boss monster works now. fixed small dungeon bug where final boss wasnt appended to exit.items list

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 from SOURCE_CODE_0307_VERSION_1.controller.game_controller import GameController
+from SOURCE_CODE_0307_VERSION_1.model.characters.warrior import Warrior
+from SOURCE_CODE_0307_VERSION_1.data.database import DatabaseManager
+from SOURCE_CODE_0307_VERSION_1.model.items.pillar import Pillar
 
-GameController()
+hero = Warrior(DatabaseManager().get_connection('SOURCE_CODE_0307_VERSION_1/data/dungeon_game.sql'))
+hero.name = 'TEST BOSS'
+hero.pillars = [Pillar('A'),Pillar('E'),Pillar('I'),Pillar('P')]
+g = GameController(hero)
+g.view.hero = hero
+g.play()
